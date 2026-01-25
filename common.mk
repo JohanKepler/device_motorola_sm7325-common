dce17acd4a22be91b93dfb13ca3112b395157740
HotaruOs <arroztlg@gmail.com>
2026-01-25 17:40:56 -0300

    sm7325-common: Switch to the pixel power-libperfmgr

--- common.mk
+++ common.mk
@@ -325,7 +325,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     libqti-perfd-client
     
 # Perf
@@ -345,7 +345,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/motorola \
     hardware/qcom-caf/common/libqti-perfd-client
 
